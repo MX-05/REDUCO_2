59704ed52fcaffb83affeccdca22163f5405686b
MX-05 <mazzeomarco9@gmail.com>
2021-10-08 16:10:04 +0200

    add get_data function to get the data from .csv file

--- reduCO2.cpp
+++ reduCO2.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+#define max 100
 
-// TODO: aggiungere tutte le categorie come variabile globale
-struct reduCO2
+// le categorie sono 11
+struct kitchen
 {
     string tag;
-    string ingredient;
-    float consumo;
+    string food[max];
+    float co_2;
 };
 
+int get_data(kitchen Fstorage[]){
+    // TODO: inizizalizzare la tabella con tutti i dati forniti dal file excel
+    int n=0;
+    ifstream data("data.csv");
+
+    if (!data){
+        cout<<"!! ERRORE NELLA LETTURA DEL FILE DATA\n";
+    }else{
+
+        string tag = "";
+        string food = "";
+        string co2 = "";
+
+        while (getline(data, food, '|')){
+            
+            getline(data, tag, '|');
+            getline(data, co2);
+
+            cout<<tag<<" | "<<food<<" | "<<co2<<endl;
+        }
+
+        data.close(); data.clear();
+    }
+
+    return n;
+}
+
+int main(){
+    kitchen food_storage[max];
+    int N = get_data(food_storage);
+
+    system("pause");
+    return 0;
+}
